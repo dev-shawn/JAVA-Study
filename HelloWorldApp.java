03bc13c15189291e1c3d3452fa2f65192761b845
audbang <audbang@218.38.137.27>
2020-12-01 20:37:43 +0900

    add java and shawn

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,6 +1,8 @@
 public class HelloWorldApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		System.out.println("Hello JAVA World!!");
+		System.out.println("Hello Shawn World!!");
 	}
 	
 }
